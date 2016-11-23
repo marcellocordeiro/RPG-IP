e991e56019c9ff7f1b23351f1e6b6007c77608a8
ccal2 <ccal2@cin.ufpe.br>
2016-11-23 23:42:48 +0000

    change stats

---
+++ data/client.h
@@ -0,0 +1,46 @@
+#include "default.h"
+
+// VARIÁVEIS GLOBAIS
+map_data map;
+
+player_data players[MAX_CLIENTS];
+player_data monsters[MAX_MONSTERS];
+
+int qnt_clients;
+
+int network_socket;
+struct sockaddr_in server_address;
+
+// INTERFACES
+char mainmenu[50][110], options[50][110];
+char battle[50][110], lose[50][110], win[50][110];
+
+int main_height, options_height;
+int battle_height, lose_height, win_height;
+
+
+// FUNÇÕES
+void game();
+int firstAlive();
+int loadFile(char *filename, char frame[50][110]);
+void loadMap(char *filename);
+void readUpd(upd_msg upd);
+void delay(unsigned int secs);
+char* color(int id);
+void printStats (int id, int ismonster);
+void printHpBar(int hp, int max_hp);
+void printChar(char *color, char c);
+void printMap();
+void printMenu(char menu[50][110], int height);
+int islegal(int x, int y, char sprite, char c);
+int mod(int x, int m);
+void loadAll ();
+void menu(int draw);
+void createRandomMap(clientInfo *info);
+
+int sendInfoToServer(clientInfo info);
+void connectToServer(const char *server_IP);
+int readTxtFromServer(char* message);
+int readUpdFromServer(upd_msg* message);
+int sendMovToServer(mov_msg message);
+int getch();
---
+++ data/server.h
@@ -0,0 +1,65 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "default.h"
+
+/// VARIÁVEIS GLOBAIS
+map_data map;
+
+player_data clients[MAX_CLIENTS]; // vetor com clientes connectados e suas informações
+int clients_connected; // quantidade de clientes connectados
+
+player_data monsters[MAX_MONSTERS];
+
+int qnt_total; // qnt de clientes + qnt de monstros
+
+int game_status;
+
+/* 
+	game status 
+	= 0 se ngm conectou
+	= 1 se esperando confirmação do "criador do game" 
+	= 2 se jogo rodando
+*/
+
+int pos_broad; // posição livre no vetor do broadcast declarado abaixo
+upd_msg map_changes[BUFFER_SIZE]; // vetor com as alterações do mapa
+
+int sock; // id do socket do server
+
+fd_set active_fd_set, read_fd_set; // variáveis auxiliares do server
+struct sockaddr_in clientname;
+
+/// FUNÇÕES
+void clientConnected(int id, clientInfo startInfo); // função que é chamada quando um cliente é connectado
+void clientMoved(int id, mov_msg mov); // função que é chamada quando cleinte manda mensagem de movimento
+void startGame(); // função que é chamada quando cliente 0 confirma o inicio do jogo
+//void MyBroadcast(char *s); // um exemplo de como mandar uma mensagem para todos os usuários
+
+void buildUpd(int id, int ismonster, int type);
+int dmg(int atk, int def);
+void battleUpd(int id, char move);
+int findPlayer(int x, int y);
+int findMonster(int x, int y);
+int islegal(int x, int y, char sprite, char c);
+int islegalMonster(int x, int y, char c);
+void initClients();
+int monsterStat(int min, int max);
+//int monsterStat(int baseStat);
+void initMonsters();
+void monsterMove();
+
+int makeSocket(uint16_t);
+int readMovFromClient(int filedes, mov_msg *message);
+int readClientInfo(int filedes, clientInfo *message);
+void sendTxtToClient(int filedes, const char msg[]);
+void sendUpdToClient(int filedes, upd_msg change);
+void init();
+void sleepServer();
+void checkConn();
+void wasClient();
+void broadcast();
+void broadcastTxt(const char msg[], int s);
+void disconnectClient(int id);
+
+#endif
